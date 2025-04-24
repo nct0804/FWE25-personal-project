65fcfac25e660c0780d627a514fc31f1af258920
Nguyen, Chi Thien <darealthien@gmail.com>
2025-04-24 11:37:35 +0200

    feat: added docker to run MongoDB

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Use the official Node.js image
+FROM node:18
+
+# Set working directory in container
+WORKDIR /app
+
+# Copy package.json and lock file
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy source code
+COPY . .
+
+# Build TypeScript to dist/
+RUN npm run build
+
+# Expose the port your app runs on
+EXPOSE 5000
+
+# Start the app
+CMD ["node", "dist/index.js"]
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Welcome to world");
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = 'mongodb://mongo:27017/';
+
+mongoose
+  .connect(MONGO_URL, { dbName: 'TravelBooking' })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err: any) => console.log('Failed to connect to MongoDB', err));
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);});
